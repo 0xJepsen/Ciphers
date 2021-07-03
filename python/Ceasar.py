88f5409f7f9c0ae0414daffd02299084f810d505
Waylon Jepsen <57912727+0xJepsen@users.noreply.github.com>
2021-07-03 15:27:32 -0600

    Update Ceasar.py

--- python/Ceasar.py
+++ python/Ceasar.py
@@ -1,11 +1,4 @@
 
-## This is a classic cipher called a Caesar Cipher. The cesaar cipher.
-# You take your message, something like "hello" and then you shift all 
-# of the letters by a certain offset. For example, chose an offset of 3 
-# and a message of "hello". Code the message by shifting each letter 3 
-# places to the left (with respect to the alphabet). So "h" becomes "e",
-# "e" becomes, "b", "l" becomes "i", and "o" becomes "l". Then the coded message is "ebiil"
-
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 punctuation = ".,?'! "
 
